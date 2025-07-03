bd17f7f37279ab9529e75a2966f518425bf195fa
Vadia-Jade <missnaturalfuzz4319@gmail.com>
2025-07-03 11:16:37 +0100

    added AI integration

--- src/java.js
+++ src/java.js
@@ -1,14 +1,26 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: "",
+    delay: 1,
+  });
+}
+
+
+
 function generatePoem(event){
     event.preventDefault();
-
-    new Typewriter("#poem", {
-        strings: " Moments like these, so often unheard",
-        autoStart: true,
-        cursor: "",
-        delay: 1,
-      });
-  
+    let instructionInput = document.querySelector("#user-instruction");
+    let apiKey ="034407t747af2dfd1c31bo02c7fc4156";
+    let context =
+     "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML<p></p> and separate each line with a <br/>. make sure to follow the user instructions do not include a title to the poem and sign the poem at the end and Not at the beginning in a <strong></strong> element 'SheCodes AI'";
+    let prompt =`User instructions: Generate poem about ${instructionInput.value}`;
    
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+   
+  
+   axios.get(apiURL).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", generatePoem);
