0013e18bfbe2a21018964f76b524fed425137cc8
Vadia-Jade <missnaturalfuzz4319@gmail.com>
2025-07-03 13:26:05 +0100

    finished project

--- src/java.js
+++ src/java.js
@@ -14,7 +14,7 @@ function generatePoem(event){
     let instructionInput = document.querySelector("#user-instruction");
     let apiKey ="034407t747af2dfd1c31bo02c7fc4156";
     let context =
-     "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML<p></p> and separate each line with a <br/>. make sure to follow the user instructions do not include a title to the poem and sign the poem at the end and Not at the beginning in a <strong></strong> element 'SheCodes AI'";
+     "You are a humorous poem expert and love to write short playful poems. Your mission is to generate a 6 line poem in basic HTML<p></p> and separate each line with a <br/>. make sure to follow the user instructions do not include a title to the poem and sign the poem at the end and Not at the beginning in a <strong></strong> element 'SheCodes AI'";
     let prompt =`User instructions: Generate poem about ${instructionInput.value}`;
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
    
