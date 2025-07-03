ad9a861ea2aa3fd6ef7a2acfade7c7189f6dce2a
Vadia-Jade <missnaturalfuzz4319@gmail.com>
2025-07-03 12:55:46 +0100

    added better UI

--- src/java.js
+++ src/java.js
@@ -16,9 +16,11 @@ function generatePoem(event){
     let context =
      "You are a romantic poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML<p></p> and separate each line with a <br/>. make sure to follow the user instructions do not include a title to the poem and sign the poem at the end and Not at the beginning in a <strong></strong> element 'SheCodes AI'";
     let prompt =`User instructions: Generate poem about ${instructionInput.value}`;
-   
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
    
+    let poemElement = document.querySelector("#poem");
+    poemElement.classList.remove("hidden");
+    poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about the ${instructionInput.value}</div>`;
   
    axios.get(apiURL).then(displayPoem);
 }
